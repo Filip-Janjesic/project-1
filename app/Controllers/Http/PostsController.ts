01c4b80ef1f1ee10c591a1a55db8ccc61261a6fa
Filip Janješić <filip.janjesic@gmail.com>
2023-07-19 19:05:46 +0200

    update
    update

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -2,7 +2,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class PostsController {
     public index = async ({ response, view }:HttpContextContract) => {
-        const html = await  view.render('posts/index')
+        const html = await  view.render('posts/index', { title: 'Post index page' })
         console.log(html)
         return html
     }
